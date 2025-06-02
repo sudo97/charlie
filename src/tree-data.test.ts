e92e555bc4445ee7880bb8bfaeacf61a2a600ea2
Illia Mikhnevych <i.mihnevich@gmail.com>
2025-06-02 18:05:04 +0300

    green phase for tree-data

--- src/tree-data.test.ts
+++ src/tree-data.test.ts
@@ -35,6 +35,9 @@ describe("treeData", () => {
   it("should handle folders", () => {
     const hotspots: Hotspot[] = [
       { file: "src/file1.txt", complexity: 1, revisions: 1 },
+      { file: "src/file2.txt", complexity: 2, revisions: 2 },
+      { file: "src/other/file3.txt", complexity: 3, revisions: 3 },
+      { file: "entirely/other/folder/file4.txt", complexity: 4, revisions: 4 },
     ];
 
     const result = treeData(hotspots);
@@ -44,7 +47,30 @@ describe("treeData", () => {
       children: [
         {
           name: "src",
-          children: [{ name: "file1.txt", complexity: 1, revisions: 1 }],
+          children: [
+            { name: "file1.txt", complexity: 1, revisions: 1 },
+            { name: "file2.txt", complexity: 2, revisions: 2 },
+            {
+              name: "other",
+              children: [{ name: "file3.txt", complexity: 3, revisions: 3 }],
+            },
+          ],
+        },
+        {
+          name: "entirely",
+          children: [
+            {
+              name: "other",
+              children: [
+                {
+                  name: "folder",
+                  children: [
+                    { name: "file4.txt", complexity: 4, revisions: 4 },
+                  ],
+                },
+              ],
+            },
+          ],
         },
       ],
     });
--- src/tree-data.ts
+++ src/tree-data.ts
@@ -1,23 +1,49 @@
 import { Hotspot } from "./hotspots";
 
-export type TreeData =
-  | {
-      name: string;
-      children: TreeData[];
-    }
-  | {
-      name: string;
-      complexity: number;
-      revisions: number;
-    };
+type Folder = {
+  name: string;
+  children: TreeData[];
+};
+
+type File = {
+  name: string;
+  complexity: number;
+  revisions: number;
+};
+
+export type TreeData = Folder | File;
 
 export function treeData(hotspots: Hotspot[]): TreeData {
-  return {
+  const root: TreeData = {
     name: "Root",
-    children: hotspots.map((hotspot) => ({
-      name: hotspot.file,
+    children: [],
+  };
+
+  for (const hotspot of hotspots) {
+    const path = hotspot.file.split("/");
+    let subTree = root;
+    while (path.length > 1) {
+      const folderName = path.shift()!;
+      let nextSubTree = subTree.children.find(
+        (child): child is Folder =>
+          child.name === folderName && "children" in child
+      )!;
+      if (!nextSubTree) {
+        nextSubTree = {
+          name: folderName,
+          children: [],
+        };
+        subTree.children.push(nextSubTree);
+      }
+      subTree = nextSubTree;
+    }
+    subTree.children.push({
+      name: path[0]!,
       complexity: hotspot.complexity,
       revisions: hotspot.revisions,
-    })),
-  };
+    });
+  }
+  console.log(root);
+
+  return root;
 }
