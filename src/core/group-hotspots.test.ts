f01caf130ac958da6721ed267c9a8be370281185
Illia Mikhnevych <i.mihnevich@gmail.com>
2025-06-05 17:41:02 +0300

    Add groupHotspots function and corresponding tests for grouping hotspots by regex. The function aggregates complexity and revisions based on provided group patterns, enhancing hotspot analysis capabilities.

---
+++ src/core/group-hotspots.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect } from "vitest";
+import { groupHotspots } from "./group-hotspots.js";
+import { Hotspot } from "./hotspots.js";
+
+describe("groupHotspots", () => {
+  function createHotspot(
+    file: string,
+    complexity: number = 10,
+    revisions: number = 10
+  ) {
+    return { file, complexity, revisions };
+  }
+
+  it("should return the same hotspots if no groups is provided", () => {
+    const hotspots = [
+      createHotspot("src/components/Button.tsx"),
+      createHotspot("src/components/Input.tsx"),
+      createHotspot("src/components/Text.tsx"),
+    ];
+    const grouped = groupHotspots(hotspots, {});
+    expect(grouped).toEqual(hotspots);
+  });
+
+  it("should group hotspots by regex", () => {
+    const hotspots = [
+      createHotspot("src/moduleA/Button.tsx", 10, 1),
+      createHotspot("src/moduleA/Input.tsx", 20, 2),
+      createHotspot("src/moduleB/Text.tsx", 30, 3),
+    ];
+    const grouped = groupHotspots(hotspots, {
+      ["^src/moduleA/"]: "moduleA",
+      ["^src/moduleB/"]: "moduleB",
+    });
+    expect(grouped).toEqual<Hotspot[]>([
+      {
+        file: "moduleA",
+        complexity: 30,
+        revisions: 3,
+      },
+      {
+        file: "moduleB",
+        complexity: 30,
+        revisions: 3,
+      },
+    ]);
+  });
+});
---
+++ src/core/group-hotspots.ts
@@ -0,0 +1,34 @@
+import type { Hotspot } from "./hotspots.js";
+
+export function groupHotspots(
+  hotspots: Hotspot[],
+  groups: Record<string, string>
+): Hotspot[] {
+  if (Object.keys(groups).length === 0) {
+    return hotspots;
+  }
+
+  const groupRegexes = Object.entries(groups).map(([key, value]) => ({
+    regex: new RegExp(key),
+    group: value,
+  }));
+
+  const grouped = hotspots.reduce((acc, curr) => {
+    const group = groupRegexes.find((group) => group.regex.test(curr.file));
+    if (group) {
+      const key = group.group;
+      if (!acc[key]) {
+        acc[key] = { revisions: 0, complexity: 0 };
+      }
+      acc[key].revisions += curr.revisions;
+      acc[key].complexity += curr.complexity;
+    }
+    return acc;
+  }, {} as Record<string, { complexity: number; revisions: number }>);
+
+  return Object.entries(grouped).map(([file, { complexity, revisions }]) => ({
+    file,
+    complexity,
+    revisions,
+  }));
+}
