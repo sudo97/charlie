65e70c2c6c3c2c1fb3bdb73d6b80fe79b383b501
Illia Mikhnevych <i.mihnevich@gmail.com>
2025-06-06 15:44:46 +0300

    Refactor hotspots-visualization.ts to improve SVG rendering and data handling. Adjust viewBox calculation for better centering, streamline zoom functionality, and enhance label and node attribute updates for improved performance and clarity.

--- src/frontend/src/hotspots-visualization.ts
+++ src/frontend/src/hotspots-visualization.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as d3 from "d3";
-import type { TreeData, Folder, File } from "@core/tree-data";
+import type { TreeData } from "@core/tree-data";
 
 function getData(): TreeData {
   const dataElement = document.getElementById("data");
@@ -20,15 +20,18 @@ const getColor = (min: number, max: number) =>
     .interpolate(d3.interpolateHcl as any);
 
 const getSvgRoot = ({ width, height }: { width: number; height: number }) => {
-  return d3
-    .create("svg")
-    .attr("viewBox", `0 0 ${width} ${height}`)
-    .attr("width", width)
-    .attr("height", height)
-    .attr(
-      "style",
-      `max-width: 100%; height: auto; display: block; margin: 0 -14px; background: ${bgColor}; cursor: pointer;`
-    );
+  return (
+    d3
+      .create("svg")
+      // .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+      .attr("width", width)
+      .attr("height", height)
+      .attr(
+        "style",
+        `max-width: 100%; height: auto; display: block; margin: 0 -14px; background: ${bgColor}; cursor: pointer;`
+      )
+  );
 };
 
 const packData = (
@@ -135,21 +138,16 @@ function getSvg(data: TreeData) {
   svg.on("click", (event) => zoom(event, root));
 
   function zoomTo(v: [number, number, number]) {
-    console.log(v);
-
     const k = width / v[2];
 
     view = v;
 
-    label.attr(
-      "transform",
-      (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
-    );
-    node.attr(
-      "transform",
-      (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
-    );
-    node.attr("r", (d) => d.r * k);
+    label.attr("x", (d) => (d.x - v[0]) * k).attr("y", (d) => (d.y - v[1]) * k);
+
+    node
+      .attr("cx", (d) => (d.x - v[0]) * k)
+      .attr("cy", (d) => (d.y - v[1]) * k)
+      .attr("r", (d) => d.r * k);
   }
 
   function zoom(event: any, d: d3.HierarchyCircularNode<TreeData>) {
@@ -158,22 +156,26 @@ function getSvg(data: TreeData) {
     const transition = svg
       .transition()
       .duration(event.altKey ? 7500 : 750)
-      .tween("zoom", (d) => {
+      .tween("zoom", () => {
         const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
         return (t) => zoomTo(i(t));
       });
 
     label
       .filter(function (d) {
-        return d.parent === focus || this.style.display === "inline";
+        return (
+          d.parent === focus ||
+          (this as SVGTextElement).style.display === "inline"
+        );
       })
-      .transition(transition)
+      .transition(transition as any)
       .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
       .on("start", function (d) {
-        if (d.parent === focus) this.style.display = "inline";
+        if (d.parent === focus)
+          (this as SVGTextElement).style.display = "inline";
       })
       .on("end", function (d) {
-        if (d.parent !== focus) this.style.display = "none";
+        if (d.parent !== focus) (this as SVGTextElement).style.display = "none";
       });
   }
 
